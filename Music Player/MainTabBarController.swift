4d8549d3f09ffa0789ae2db97e171d8e54f613d3
ASaidaxmad <saidaxmad7760746@gmail.com>
2022-09-29 09:51:37 +0500

    Setup Tab Bar and Nav Bar Controller

---
+++ Music Player/MainTabBarController.swift
@@ -0,0 +1,38 @@
+//
+//  MainTabBarController.swift
+//  Music Player
+//
+//  Created by Saidaxmad on 28/09/22.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        tabBar.backgroundColor = .systemGray6
+        
+        tabBar.tintColor = #colorLiteral(red: 1, green: 0.1719063818, blue: 0.4505617023, alpha: 1)
+
+        
+        viewControllers = [
+            generateViewController(rootViewController: SearchViewController(), image: "magnifyingglass", title: "Search"),
+            generateViewController(rootViewController: ViewController(), image: "music.note.list", title: "Library")
+        ]
+    }
+    
+    private func generateViewController(rootViewController: UIViewController, image: String, title: String) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.image = UIImage(systemName: image)
+        navigationVC.tabBarItem.title = title
+        rootViewController.navigationItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        return navigationVC
+    }
+}
+
+// music.note.list
+// magnifyingglass
--- Music Player/SearchViewController.swift
+++ Music Player/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  Music Player
 //
 //  Created by Saidaxmad on 28/09/22.
@@ -7,14 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SearchViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
     }
-
-
 }
-
