2b4564d3ba2a04a90944b8d1bfcb82f10d1792f3
ASaidaxmad <saidaxmad7760746@gmail.com>
2022-09-30 15:47:37 +0500

    Create Xib file TrackCell

---
+++ Music Player/Search/Cell/TrackCell.swift
@@ -0,0 +1,53 @@
+//
+//  TrackCell.swift
+//  Music Player
+//
+//  Created by Saidaxmad on 30/09/22.
+//
+
+import UIKit
+import SDWebImage
+
+protocol TrackCellViewModel {
+    var iconUrlString: String? { get }
+    var trackName: String { get }
+    var artistName: String { get }
+    var collectionName: String { get }
+}
+
+class TrackCell: UITableViewCell {
+
+    static let reuseId = "TrackCell"
+    
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var trackNameLabel: UILabel!
+    
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var collectionNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        trackImageView.layer.cornerRadius = 15
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        trackImageView.image = nil
+    }
+    
+    func set(viewModel: TrackCellViewModel) {
+        
+        trackNameLabel.text = viewModel.trackName
+        artistNameLabel.text = viewModel.artistName
+        collectionNameLabel.text = viewModel.collectionName
+        
+        guard let url = URL(string: viewModel.iconUrlString ?? "") else { return }
+        trackImageView.sd_setImage(with: url, completed: nil)
+    }
+}
--- Music Player/Search/SearchViewController.swift
+++ Music Player/Search/SearchViewController.swift
@@ -12,6 +12,7 @@
 
 import UIKit
 
+
 protocol SearchDisplayLogic: AnyObject {
     func displaySomething(viewModel: Search.Something.ViewModel.ViewModelData)
 }
@@ -23,7 +24,8 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
     @IBOutlet weak var table: UITableView!
     
     let searchController = UISearchController(searchResultsController: nil)
-
+    private var searchViewModel = SearchViewModel.init(cells: [])
+    private var timer: Timer?
     
     // MARK: Setup
     
@@ -75,10 +77,14 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
         searchController.searchBar.delegate = self
+        searchController.obscuresBackgroundDuringPresentation = false
     }
     
     private func setupTableView() {
         table.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        
+        let nib = UINib(nibName: "TrackCell", bundle: nil)
+        table.register(nib, forCellReuseIdentifier: TrackCell.reuseId)
     }
     
     func displaySomething(viewModel: Search.Something.ViewModel.ViewModelData) {
@@ -88,6 +94,8 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
             print("view ok")
         case .displayTacks(let searchViewModel):
             print("display")
+            self.searchViewModel = searchViewModel
+            table.reloadData()
         }
     }
 }
@@ -98,13 +106,19 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return searchViewModel.cells.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
-        cell.textLabel?.text = "indexPath"
+        let cell = tableView.dequeueReusableCell(withIdentifier: TrackCell.reuseId, for: indexPath) as! TrackCell
+        let cellViewModel = searchViewModel.cells[indexPath.row]
+        
+        cell.set(viewModel: cellViewModel)
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        84
     }
 }
 
@@ -112,6 +126,9 @@ extension SearchViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
-        interactor?.doSomething(request: Search.Something.Request.RequestType.getTracks(searchTerm: searchText))
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
+            self.interactor?.doSomething(request: Search.Something.Request.RequestType.getTracks(searchTerm: searchText))
+        })
     }
 }
